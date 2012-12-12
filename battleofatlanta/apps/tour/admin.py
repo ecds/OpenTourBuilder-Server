caf40a456e136d72050244c5f591c511dd963235
Kyle Bock <kylewbock@gmail.com>
2012-12-12 17:36:14 -0500

    added some missing icons. also added js that loads on teh tour admin that will collapse the stacked inline forms

--- battleofatlanta/apps/tour/admin.py
+++ battleofatlanta/apps/tour/admin.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 
 from battleofatlanta.apps.tour.models import Tour, TourStop
 
-class TourStopInline(admin.TabularInline):
+class TourStopInline(admin.StackedInline):
     model = TourStop
     extra = 1
 
 class TourAdmin(admin.ModelAdmin):
     inlines = ( TourStopInline, )
+
+    # collapse our stacked inlines by default
+    class Media:
+        js = ('/static/js/admin/collapse-inlines.js',)
 
 admin.site.register(Tour, TourAdmin)
 admin.site.register(TourStop)
---
+++ battleofatlanta/sitemedia/js/admin/collapse-inlines.js
@@ -0,0 +1,4 @@
+// used to collapse the inlines in the admin panel
+$(function() {
+    $(".collapse").removeClass("in");
+})
