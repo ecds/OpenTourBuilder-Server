8616f0c4a23d4a20217e89cc1ab67857618ec84a
Kyle Bock <kylewbock@gmail.com>
2013-01-07 15:49:07 -0500

    This finished the basic test implementation
    Will add more tests as we build out more functionality

--- battleofatlanta/apps/tour/tests/models.py
+++ battleofatlanta/apps/tour/tests/models.py
@@ -18,20 +18,21 @@ from battleofatlanta.apps.tour.models import Tour, TourStop
 logger = logging.getLogger(__name__)
 
 class TourTestCase(DjangoTestCase):
-    fixtures = ["tours.json"]
+    fixtures = ["tours.json", "tour_stops.json"]
 
     def setUp(self):
-        logger.info("Tours: %s" % Tour.objects.all().count())
-
-        self.tour = Tour()
-        self.tour.name = "Battle of Atlanta"
-        self.tour.description = "A tour of the Battle of Atlanta"
+        # get the tour
+        self.tour = Tour.objects.get(pk=1)
 
     def test_init(self):
         logger.info("Tours: %s" % Tour.objects.all().count())
         nt.assert_true(isinstance(self.tour, Tour))
         nt.assert_equal(self.tour.name, "Battle of Atlanta")
         nt.assert_equal(self.tour.description, "A tour of the Battle of Atlanta")
+
+    def test_tour_stops(self):
+        nt.assert_true(self.tour.tourstop_set.all().count > 0)
+
 
 class TourStopTestCase(object):
     def setup(self):
