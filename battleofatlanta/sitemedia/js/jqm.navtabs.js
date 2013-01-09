e9679eab03928001e512af69f622896885326c5e
Kyle Bock <kylewbock@gmail.com>
2013-01-09 14:47:16 -0500

    working jqm.navtabs
    todo: can we rebuild this js as a jquery mobile extension?

--- battleofatlanta/sitemedia/js/jqm.navtabs.js
+++ battleofatlanta/sitemedia/js/jqm.navtabs.js
@@ -1,11 +1,11 @@
 $(document).delegate('.ui-navbar ul li > a', 'click', function() {
-  //un-highlight and highlight only the buttons in the same navbar widget
+  //search the navbar to deactivate the active button
   $(this).closest('.ui-navbar').find('a').removeClass('ui-btn-active');
 
-  //this bit is the same, you could chain it off of the last call by using two `.end()`s
+  //change the active tab
   $(this).addClass('ui-btn-active');
 
-  //this starts the same but then only selects the sibling `.tab_content` elements to hide rather than all in the DOM
+  //hide the siblings
   $('#' + $(this).attr('data-href')).show().siblings('.tab-content').hide();
 
   return false;
