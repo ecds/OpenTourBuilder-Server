9deeb2009c50a0862c3b8ed405a12991da7dabb8
Kyle Bock <kylewbock@gmail.com>
2013-01-17 13:59:27 -0500

    adding test settings

--- battleofatlanta/settings/__init__.py
+++ battleofatlanta/settings/__init__.py
@@ -1,9 +1,19 @@
 # file battleofatlanta/settings/__init__.py
 
 from .base import *
-try:
-    from .local import *
-except ImportError, exc:
-    exc.args = tuple(
-        ['%s (did you rename settings/local-dist.py?)' % exc.args[0]])
-    raise exc
+
+if not 'test' in sys.argv:
+    try:
+        from .local import *
+    except ImportError, exc:
+        exc.args = tuple(
+            ['%s (did you rename settings/local-dist.py?)' % exc.args[0]])
+        raise exc
+
+if 'test' in sys.argv:
+    try:
+        from .test import *
+    except ImportError, exc:
+        exc.args = tuple(
+            ['%s (did you rename settings/test.py?)' % exc.args[0]])
+        raise exc
