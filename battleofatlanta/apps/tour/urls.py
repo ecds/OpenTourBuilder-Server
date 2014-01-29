80409bd7b61b405ec69a9781ca6a03213c61703a
Jay Varner <jayvarner@gmail.com>
2014-01-29 16:58:06 -0500

    Added view to handle session value for directions mode.

--- battleofatlanta/apps/tour/urls.py
+++ battleofatlanta/apps/tour/urls.py
@@ -3,8 +3,10 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('battleofatlanta.apps.tour.views',
+    url(r'^update_directionsmode/(?P<mode>[-\w]+)/$', 'update_directionsmode', name='mode'),
     url(r'^(?P<slug>[-\w]+)/$', 'tour_detail', name='detail'),
     url(r'^(?P<slug>[-\w]+)/(?P<page>\d+)/$', 'tour_stop_detail', name='stop-detail'),
-    url(r'^(?P<slug>[-\w]+)/(?P<info>\w+)/$', 'tour_info_detail', name='info-detail'),
+    url(r'^(?P<slug>[-\w]+)/(?P<info>[-\w]+)/$', 'tour_info_detail', name='info-detail'),
     url(r'^(?P<slug>[-\w]+)/media/(?P<id>\d+)/$', 'tour_stop_media_detail', name='stop-media-detail'),
+#    url(r'^update_directionsmode/(?P<mode>[-\w]+)/$', 'update_directionsmode', name='switch'),
 )
--- battleofatlanta/apps/tour/views.py
+++ battleofatlanta/apps/tour/views.py
@@ -4,7 +4,24 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.core.paginator import Paginator
 from django.http import HttpResponseRedirect
-from battleofatlanta.apps.tour.models import Tour, TourInfo, TourStop, TourStopMedia
+from django.http import HttpResponse
+from django.core.context_processors import csrf
+#from django.views.decorators.csrf import csrf_protect
+from battleofatlanta.apps.tour.models import Tour, TourInfo, TourStop, TourStopMedia, DirectionsMode
+
+def directions(request, slug):
+    tour = tour = get_object_or_404(Tour, slug=slug)
+    if  "directions" not in request.session:
+        mode = str(tour.mode)
+        print("Mode = " + mode)
+        request.session["directions"] = mode
+
+def update_directionsmode(request, mode):
+    if not request.is_ajax() or not request.method=='POST':
+        return HttpResponseNotAllowed(['POST'])
+
+    request.session['directions'] = mode
+    return HttpResponse('ok')
 
 def tour_detail(request, slug):
     tour = get_object_or_404(Tour, slug=slug)
@@ -19,7 +36,7 @@ def tour_detail(request, slug):
 
 def tour_info_detail(request, slug, info):
     tour = get_object_or_404(Tour, slug=slug)
-    tour_info = tour.tourinfo_set.filter(name=info)
+    tour_info = tour.tourinfo_set.filter(info_slug=info)
 
 
     return render_to_response("tour/tour_info-detail.html", {
@@ -28,18 +45,31 @@ def tour_info_detail(request, slug, info):
             'tour_info': tour_info[0],
         }, context_instance=RequestContext(request))
 
+#@csrf_protect
 def tour_stop_detail(request, slug, page):
     tour = get_object_or_404(Tour, slug=slug)
+
+#    c = {}
+#    c.update(csrf(request))
+
+    directions(request, slug)
 
     paginator = Paginator(tour.tourstop_set.all(), 1)
 
     page = paginator.page(int(page))
+
+    directions_pref = request.session["directions"]
+
+    modes = DirectionsMode.objects.all()
 
     return render_to_response("tour/tour_stop-detail.html", {
             'tour': tour,
             'tour_stop': page[0],
             'images': page[0].tourstopmedia_set.all(),
             'page': page,
+            'directions': directions_pref,
+            'modes': modes,
+#            'csrf_token': c,
         }, context_instance=RequestContext(request))
 
 def tour_stop_media_detail(request, slug, id):
