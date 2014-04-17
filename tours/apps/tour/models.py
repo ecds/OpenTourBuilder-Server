b51605fd901dbb118e8ba78abc35f794b99a39ee
Jay Varner <jayvarner@gmail.com>
2014-04-17 15:25:06 -0400

    Added directions intro.

--- tours/apps/tour/models.py
+++ tours/apps/tour/models.py
@@ -97,7 +97,7 @@ class TourStop(models.Model):
     park_lat = models.FloatField(null=True, blank=True)
     park_lng = models.FloatField(null=True, blank=True)
     parking_block = models.TextField(blank=True, default='')
-
+    directions_intro = models.TextField(blank=True, default='', validators=[MaxLengthValidator(350)])
     direction_notes = HTMLField(blank=True, default='')
 
     # used in drag and drop reodering as well as tour stop order
